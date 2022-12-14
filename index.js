f461cbc78f1be8ecedbbdb85c3a4e6075ab03579
Alvin Mithamo <alvinmithamo100@gmail.com>
2022-12-14 10:54:05 +0300

    object lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+let employee = {
+  name: "William Lawson",
+  streetAddress: "1342 Pandhi Stret"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  let updatedEmployee = {...employee, [key]: value};
+  return updatedEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+  let {[key]: deleted, ...updatedEmployee} = employee;
+  return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
